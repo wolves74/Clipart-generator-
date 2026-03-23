67553682990e66af94458f8f8a72e2b47b26e627
wolves74 <tams74@msn.com>
2026-03-23 09:35:40 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "clipart-prompt-v1";
+const URLS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./farm-logo.png",
+  "./app-icon.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
